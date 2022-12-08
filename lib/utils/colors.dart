d555ac5afc6a9c87af80ef844ad48de9711c5012
sherzodkamoldinov <sherzodkamoldinov1999@gmail.com>
2022-12-09 01:35:22 +0500

    added utils

---
+++ lib/utils/colors.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class MyColors {
+  static const Color backgroundColor = Color(0xFF121212);
+  static const Color buttonColor = Color(0xFF8687E7);
+  static Color fontColor = Colors.white.withOpacity(0.87);
+  static const Color dialogColor = Color(0xFF363636);
+  static const Color textFieldColor = Color(0xFF979797);
+  static const Color borderColor = Color(0xFF1D1D1D);
+}
---
+++ lib/utils/text_style.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+import 'package:todo_app/utils/colors.dart';
+
+class MyTextStyle {
+  static TextStyle regularLato = TextStyle(
+    fontFamily: 'Lato',
+    fontSize: 16,
+    color: MyColors.fontColor,
+    fontWeight: FontWeight.w400,
+  );
+
+  static TextStyle thinLato = TextStyle(
+    fontFamily: 'Lato',
+    fontSize: 16,
+    color: MyColors.fontColor,
+    fontWeight: FontWeight.w100,
+  );
+
+  static TextStyle lightLato = TextStyle(
+    fontFamily: 'Lato',
+    fontSize: 16,
+    color: MyColors.fontColor,
+    fontWeight: FontWeight.w300,
+  );
+
+  static TextStyle boldLato = TextStyle(
+    fontFamily: 'Lato',
+    fontSize: 16,
+    color: MyColors.fontColor,
+    fontWeight: FontWeight.w700,
+  );
+
+  static TextStyle blackLato = TextStyle(
+    fontFamily: 'Lato',
+    fontSize: 16,
+    color: MyColors.fontColor,
+    fontWeight: FontWeight.w900,
+  );
+}
